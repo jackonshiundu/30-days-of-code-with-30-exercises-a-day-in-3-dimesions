c6300ddc39f1c139b9d239b87ad678a5e77f82df
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-08 23:55:40 +0300

    find-index

---
+++ Day_27_30_3/5.js
@@ -0,0 +1,14 @@
+//find index
+function deleteCustomerById(customers, value) {
+    var index = customers.findIndex((customer) => customer.id === value);  // Use findIndex to find the index
+    if (index > -1) {
+        customers.splice(index, 1);  // Use splice to remove the element at that index
+    }
+    return customers;
+}
+
+const customers = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Jack"},
+    {id: 3, name: "Thony"}
+];
